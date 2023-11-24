58dd7e873c1797fd92a5579729018b7e54b9a537
0emerald <91470157+0emerald@users.noreply.github.com>
2023-11-24 15:14:39 +0000

    Add files via upload

---
+++ 01Bash/lookup_bash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script to create a lookup file of postcode and host
+# could include year aswell, but it means my laptop runs out of memory
+
+# Uses    all.1.csv    file
+
+cut -d, -f 2,4 all.1.csv > hosts-pc.csv
+
+# to execute, navigate in terminal to where this bash script is saved and then execute
+# chmod +x lookup_bash.sh
+# ./lookup_bash.sh
+
